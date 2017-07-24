ee638d6c8ed84009c50b971b8975ecce2a566339
Wall-C <wacypcarz@gmail.com>
2017-07-24 11:54:09 +0200

    Article index, show and create controllers and view in progress

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,21 @@
+class ArticlesController < ApplicationController
+
+  def index 
+  	@articles = Article.all
+  end
+
+  def show
+  	@article = Article.find(params[:id])
+  end
+
+
+
+
+
+
+end
+
+
+
+
+
